16587e4d350126dd227adb57d876553678106440
lukas <pichlmannlukas@gmail.com>
2021-06-25 00:16:16 +0200

    a lot of modifications and restructuring

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import re
 import argparse
 from collections import deque
+import gc
 
 import numpy as np
 import torch
@@ -19,7 +20,8 @@ from src.rollout_storage import RolloutStorage
 from helpers import pretrained_model
 
 update_factor = config.num_steps * config.num_processes
-num_updates = int(config.num_frames) // update_factor
+#num_updates = int(config.num_frames) // update_factor
+num_updates = 500
 lr_update_schedule = None if config.lr_schedule is None else config.lr_schedule // update_factor
 
 torch.manual_seed(config.seed)
@@ -55,22 +57,6 @@ def main():
     tensorboard_x_data_points_counts = 0
 
     print(f"Using {config.num_processes} processes")
-    envs = make_vec_envs(
-        config.env_name, config.seed, config.num_processes, config.gamma, config.no_norm, config.num_stack,
-        config.log_dir, config.add_timestep, device, allow_early_resets=False
-    )
-
-    if config.eval_interval:
-        eval_envs = make_vec_envs(
-            config.env_name, config.seed + config.num_processes, config.num_processes, config.gamma,
-            config.no_norm, config.num_stack, eval_log_dir, config.add_timestep, device,
-            allow_early_resets=True, eval=True
-        )
-
-        if eval_envs.venv.__class__.__name__ == "VecNormalize":
-            eval_envs.venv.ob_rms = envs.venv.ob_rms
-    else:
-        eval_envs = None
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -81,7 +67,6 @@ def main():
     args = parser.parse_args()
     start_update = 0
     if args.path:
-        print('-------------\nLoading checkpoint:', args.path, '\n-------------')
         pattern = re.match(r'.*GraphicOVOCompact-v0_(\d+).pt', args.path)
         if pattern:
             start_update = int(pattern.group(1))
@@ -97,121 +82,176 @@ def main():
         max_grad_norm=config.max_grad_norm
     )
 
-    rollouts = RolloutStorage(
-        config.num_steps, config.num_processes,
-        envs.observation_space.shape,
-        envs.action_space,
-        actor_critic.recurrent_hidden_state_size
-    )
+    while True:
+        if 'envs' in locals():
+            print("\n-------------------------")
+            print("Resetting environment...")
+            print("-------------------------\n")
+            del envs
+            gc.collect()
 
-    obs = envs.reset()
-    rollouts.obs[0].copy_(obs)
-    rollouts.to_device(device)
+        print("\nCreating Training Environments...\n")
+        envs = make_vec_envs(
+            config.env_name, config.seed, config.num_processes, config.gamma, config.no_norm, config.num_stack,
+            config.log_dir, config.add_timestep, device, allow_early_resets=False
+        )
 
-    episode_rewards = deque(maxlen=10)
+        if config.eval_interval:
+            print("\nCreating Eval Environments...\n")
+            eval_envs = make_vec_envs(
+                config.env_name, config.seed + config.num_processes, config.num_processes, config.gamma,
+                config.no_norm, config.num_stack, eval_log_dir, config.add_timestep, device,
+                allow_early_resets=True, eval=True
+            )
 
-    start = time.time()
-    times_since_bomb = torch.zeros((config.num_processes,1), dtype=torch.int32)
-    last_blast_str = torch.zeros((config.num_processes,1), dtype=torch.int32) + 2
-    last_max_ammo = torch.ones((config.num_processes,1), dtype=torch.int32) 
-    for j in range(start_update, start_update+num_updates):
-        for step in range(config.num_steps):
-            # Sample actions
-            with torch.no_grad():
-                value, action, action_log_prob, recurrent_hidden_states = actor_critic.act(
-                    rollouts.obs[step],
-                    rollouts.recurrent_hidden_states[step],
-                    rollouts.masks[step])
+            if eval_envs.venv.__class__.__name__ == "VecNormalize":
+                eval_envs.venv.ob_rms = envs.venv.ob_rms
+        else:
+            eval_envs = None
 
-            # Obser reward and next obs
-            obs, reward, done, infos, blast_str, ammo = envs.step(action)
+        rollouts = RolloutStorage(
+            config.num_steps, config.num_processes,
+            envs.observation_space.shape,
+            envs.action_space,
+            actor_critic.recurrent_hidden_state_size
+        )
 
-            for info in infos:
-                if 'episode' in info.keys():
-                    episode_rewards.append(info['episode']['r'])
+        obs = envs.reset()
+        rollouts.obs[0].copy_(obs)
+        rollouts.to_device(device)
 
-            blast_str = torch.as_tensor(blast_str, dtype=torch.int32)[:,None]
-            ammo = torch.as_tensor(ammo, dtype=torch.int32)[:,None]
+        episode_rewards = deque(maxlen=10)
 
-            # give reward for collecting power-up
-            idx_blast_incr = blast_str > last_blast_str
-            if idx_blast_incr.sum(): print("Hooray! Blast Strength increased!")
-            reward[idx_blast_incr] += 0.5
-            last_blast_str = blast_str 
-
-            # give reward for collecting ammo-up
-            idx_ammo_incr = ammo > last_max_ammo
-            if idx_ammo_incr.sum(): print("Hooray! Ammo increased!")
-            reward[idx_ammo_incr] += 0.5
-            last_max_ammo[idx_ammo_incr] = ammo[idx_ammo_incr]
-
-            # decrease reward if our agent CAN NOT be responsible for the opponents death
-            idx_bomb_kill = (times_since_bomb<10) * (reward.cpu()>0)
-            reward[~idx_bomb_kill] *= 0.5
-
-            # If done then clean the history of observations.
-            masks = torch.tensor([[0.0] if done_ else [1.0] for done_ in done], device=device)
-            rollouts.insert(obs, recurrent_hidden_states, action, action_log_prob, value, reward, masks)
-
-            times_since_bomb[action==5] = 0
-            times_since_bomb += 1
-
-        with torch.no_grad():
-            next_value = actor_critic.get_value(rollouts.obs[-1],
-                                                rollouts.recurrent_hidden_states[-1],
-                                                rollouts.masks[-1]).detach()
-
-        rollouts.compute_returns(next_value, config.use_gae, config.gamma, config.tau)
-
-        value_loss, action_loss, dist_entropy, other_metrics = agent.update(rollouts, j)
-
-        rollouts.after_update()
-
-        if j % config.save_interval == 0 and config.save_dir != "":
-            save_path = os.path.join(config.save_dir)
-            os.makedirs(save_path, exist_ok=True)
-            state_dict = actor_critic.state_dict() if device.type == "cpu" else actor_critic.state_dict()
-            save_model = [state_dict, hasattr(envs.venv, 'ob_rms') and envs.venv.ob_rms or None]
-
-            torch.save(save_model, os.path.join(save_path, config.env_name + f"_{j}.pt"))
-
-        total_num_steps = (j + 1) * update_factor
-
-        if j % config.log_interval == 0 and len(episode_rewards) > 1:
-            end = time.time()
-
-            print(f"Updates={j}, timesteps={total_num_steps}, FPS {total_num_steps // (end - start)} "
-                  f"last {len(episode_rewards)}, mean {np.mean(episode_rewards):.3f} "
-                  f"value/action loss {value_loss:.3f}/{action_loss:.3f}, entropy {dist_entropy:.3f}")
-
-            tensorboard.add_scalar("MeanReward", np.mean(episode_rewards), tensorboard_x_data_points_counts)
-            tensorboard.add_scalar("Entropy", dist_entropy, tensorboard_x_data_points_counts)
-            tensorboard.add_scalar("ValueLoss", value_loss, tensorboard_x_data_points_counts)
-            tensorboard.add_scalar("ActionLoss", action_loss, tensorboard_x_data_points_counts)
-            tensorboard_x_data_points_counts += 1
-
-        if config.eval_interval and len(episode_rewards) > 1 and j > 0 and j % config.eval_interval == 0:
-            eval_episode_rewards = []
-
-            obs = eval_envs.reset()
-            eval_recurrent_hidden_states = torch.zeros(config.num_processes, actor_critic.recurrent_hidden_state_size,
-                                                       device=device)
-            eval_masks = torch.zeros(config.num_processes, 1, device=device)
-
-            while len(eval_episode_rewards) < 50:
+        start = time.time()
+        times_since_bomb = torch.zeros((config.num_processes,1), dtype=torch.int32)
+        times_since_done = torch.zeros((config.num_processes,1), dtype=torch.int32)
+        last_blast_str = torch.zeros((config.num_processes,1), dtype=torch.int32) + 2
+        last_max_ammo = torch.ones((config.num_processes,1), dtype=torch.int32) 
+        for j in range(start_update, start_update+num_updates):
+            for step in range(config.num_steps):
+                # Sample actions
                 with torch.no_grad():
-                    _, action, _, eval_recurrent_hidden_states = actor_critic.act(
-                        obs, eval_recurrent_hidden_states, eval_masks, deterministic=True)
+                    value, action, action_log_prob, recurrent_hidden_states = actor_critic.act(
+                        rollouts.obs[step],
+                        rollouts.recurrent_hidden_states[step],
+                        rollouts.masks[step])
 
                 # Obser reward and next obs
-                obs, reward, done, infos, _, _ = eval_envs.step(action)
-                eval_masks = torch.tensor([[0.0] if done_ else [1.0] for done_ in done], device=device)
+                obs, reward, done, infos, blast_str, ammo = envs.step(action)
+
                 for info in infos:
                     if 'episode' in info.keys():
-                        eval_episode_rewards.append(info['episode']['r'])
+                        episode_rewards.append(info['episode']['r'])
 
-            print(f"Games played: {len(eval_episode_rewards)}, mean reward {np.mean(eval_episode_rewards):.5f}\n")
+                blast_str = torch.as_tensor(blast_str, dtype=torch.int32)[:,None]
+                ammo = torch.as_tensor(ammo, dtype=torch.int32)[:,None]
+
+                # give reward for collecting power-up
+                idx_blast_incr = blast_str > last_blast_str
+                if idx_blast_incr.sum(): print(" > Hooray! Blast Strength increased!")
+                reward[idx_blast_incr] += 0.5
+                last_blast_str = blast_str 
+
+                # give reward for collecting ammo-up
+                idx_ammo_incr = ammo > last_max_ammo
+                if idx_ammo_incr.sum(): print(" > Hooray! Ammo increased!")
+                reward[idx_ammo_incr] += 0.5
+                last_max_ammo[idx_ammo_incr] = ammo[idx_ammo_incr]
+
+                # decrease reward if our agent CAN NOT be responsible for the opponents death
+                idx_bomb_kill = (times_since_bomb<10) * (reward.cpu()>0)
+                reward[~idx_bomb_kill] *= 0.25
+
+                ## punish not laying bombs for too long
+                #idx_random_nobomb_thr = (times_since_bomb >= 20 + np.random.randint(0, 5))
+                #reward[idx_random_nobomb_thr] -= 0.05
+
+                ## small random reward for placing bombs
+                #idx_random_bomb_reward = (times_since_bomb >= (10 + np.random.randint(0, 3))) * action.cpu()==5
+                #reward[idx_random_bomb_reward] += 0.05
+
+                ## punish repeating patterns (lead to draws)
+                #b = rollouts.actions.squeeze()
+                #pattern_len = 4
+                #c = b[-pattern_len:,:]
+                #d = b[:-pattern_len,:]
+                #
+                #z = [[(c[:,j]==d[i:(i+c.shape[0]),j]).all() for i in range(d.shape[0]-c.shape[0])] for j in range(b.shape[1])]
+                #z = torch.as_tensor(z)
+                #
+                #idx_repeats = (z.sum(1) >= 2)[:,None] * (times_since_done >= pattern_len*3)
+                #if idx_repeats.sum(): print(" > Punishing repeating behaviour...")
+                #reward[idx_repeats] -= 0.25
+
+                ## If done then clean the history of observations.
+                #masks = torch.tensor([[0.0] if done_ else [1.0] for done_ in done], device=device)
+                #rollouts.insert(obs, recurrent_hidden_states, action, action_log_prob, value, reward, masks)
+
+                times_since_bomb[action==5] = 0
+                times_since_bomb += 1
+                times_since_done[done] = 0
+                times_since_done += 1
+
+
+            with torch.no_grad():
+                next_value = actor_critic.get_value(rollouts.obs[-1],
+                                                    rollouts.recurrent_hidden_states[-1],
+                                                    rollouts.masks[-1]).detach()
+
+            rollouts.compute_returns(next_value, config.use_gae, config.gamma, config.tau)
+
+            value_loss, action_loss, dist_entropy, other_metrics = agent.update(rollouts, j)
+
+            rollouts.after_update()
+
+            if j % config.save_interval == 0 and config.save_dir != "":
+                save_path = os.path.join(config.save_dir)
+                os.makedirs(save_path, exist_ok=True)
+                state_dict = actor_critic.state_dict()
+                save_model = [state_dict, hasattr(envs.venv, 'ob_rms') and envs.venv.ob_rms or None]
+
+                torch.save(save_model, os.path.join(save_path, config.env_name + f"_{j}.pt"))
+
+            total_num_steps = (j + 1) * update_factor
+
+            if j % config.log_interval == 0 and len(episode_rewards) > 1:
+                end = time.time()
+
+                print(f"Updates={j}, timesteps={total_num_steps}, FPS {total_num_steps // (end - start)} "
+                      f"last {len(episode_rewards)}, mean {np.mean(episode_rewards):.3f} "
+                      f"value/action loss {value_loss:.3f}/{action_loss:.3f}, entropy {dist_entropy:.3f}")
+
+                tensorboard.add_scalar("MeanReward", np.mean(episode_rewards), tensorboard_x_data_points_counts)
+                tensorboard.add_scalar("Entropy", dist_entropy, tensorboard_x_data_points_counts)
+                tensorboard.add_scalar("ValueLoss", value_loss, tensorboard_x_data_points_counts)
+                tensorboard.add_scalar("ActionLoss", action_loss, tensorboard_x_data_points_counts)
+                tensorboard_x_data_points_counts += 1
+
+            if config.eval_interval and len(episode_rewards) > 1 and j > 0 and j % config.eval_interval == 0:
+                eval_episode_rewards = []
+
+                obs = eval_envs.reset()
+                eval_recurrent_hidden_states = torch.zeros(config.num_processes, actor_critic.recurrent_hidden_state_size,
+                                                           device=device)
+                eval_masks = torch.zeros(config.num_processes, 1, device=device)
+
+                while len(eval_episode_rewards) < 50:
+                    with torch.no_grad():
+                        _, action, _, eval_recurrent_hidden_states = actor_critic.act(
+                            obs, eval_recurrent_hidden_states, eval_masks, deterministic=True)
+
+                    # Obser reward and next obs
+                    obs, reward, done, infos, _, _ = eval_envs.step(action)
+                    eval_masks = torch.tensor([[0.0] if done_ else [1.0] for done_ in done], device=device)
+                    for info in infos:
+                        if 'episode' in info.keys():
+                            eval_episode_rewards.append(info['episode']['r'])
+
+                print(f"Games played: {len(eval_episode_rewards)}, mean reward {np.mean(eval_episode_rewards):.5f}\n")
+
+        start_update += num_updates
 
 
 if __name__ == "__main__":
     main()
+
