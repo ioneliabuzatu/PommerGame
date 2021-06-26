d35a1deeafb4cc1da47fb01900e0ed09654d577a
Ionelia Buzatu <buzatuionelia@gmail.com>
2021-06-26 14:39:42 +0200

    check for cuda in pretrained model

--- helpers/pretrained_model.py
+++ helpers/pretrained_model.py
@@ -5,7 +5,7 @@ from src.models.policy import Policy
 import numpy as np
 import torch
 
-def load_pretrained(train=True, path=None, device="cpu"):
+def load_pretrained(train=True, path=None, cuda=torch.cuda.is_available()):
     obs_space = Box(np.zeros(13440), np.ones(13440), dtype=np.float32)
     action_space = Discrete(6)
     nn_kwargs = {'batch_norm': True, 'recurrent': False, 'hidden_size': 512, 'cnn_config': 'conv5', }
@@ -20,7 +20,7 @@ def load_pretrained(train=True, path=None, device="cpu"):
         print(f"loading model for EVALUATION: {path}")
         actor_critic = Policy(PommNet(obs_shape=obs_space.shape, **nn_kwargs).eval(), action_space=action_space)
 
-    if device == "cpu":
+    if not cuda:
         actor_critic.load_state_dict(torch.load(path, map_location=torch.device('cpu') )[0])
     else:
         actor_critic.load_state_dict(torch.load(path)[0])
